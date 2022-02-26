c2b16c5b1554ae54960c6033ab1038a7a9ad408f
Dilip Agheda <dilip.agheda@origin.com.au>
2022-02-26 21:31:56 +1100

    Added a label and a button for sign up on login view

--- OnTheMap/ViewControllers/LoginViewController.swift
+++ OnTheMap/ViewControllers/LoginViewController.swift
@@ -11,14 +11,29 @@ class LoginViewController: UIViewController {
 
     @IBOutlet weak var loginButton: UIButton!
     
+    @IBOutlet weak var signUpButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        self.loginButton.titleLabel?.textColor = UIColor.white
+        let blueColor = UIColor(red: 74/255, green: 163/255, blue: 221/255, alpha: 1.0)
         self.loginButton.setTitleColor(UIColor.white, for: UIControl.State.normal)
-        self.loginButton.backgroundColor = UIColor(red: 74/255, green: 163/255, blue: 221/255, alpha: 1.0)
+        self.loginButton.backgroundColor = blueColor
+        
+        self.signUpButton.setTitleColor(blueColor, for: UIControl.State.normal)
     }
 
 
+    @IBAction func onLoginTap(_ sender: Any) {
+        
+        print("Login tapped")
+    }
+    
+    @IBAction func onSignUpTap(_ sender: Any) {
+        
+        print("Sign up tapped")
+    }
+    
 }
 
