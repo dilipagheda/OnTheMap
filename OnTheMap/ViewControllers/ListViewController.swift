ff712b249eec302a823880eb72ba30a2dc9c22ef
Dilip Agheda <dilip.agheda@origin.com.au>
2022-02-27 19:19:11 +1100

    Added navigation controller and related refactor. setup skeleton for table view controller

--- OnTheMap/ViewControllers/ListViewController.swift
+++ OnTheMap/ViewControllers/ListViewController.swift
@@ -8,7 +8,37 @@
 import Foundation
 import UIKit
 
-class ListViewController: UIViewController {
+class ListViewController: UIViewController, UITableViewDelegate {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 10
+    }
+    
+    private func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "onTheMapTableCell", for: indexPath)
+            
+        //let meme = memes[indexPath.row]
+        
+        //cell.memeImageView.image = meme.memedImage
+        //cell.memeLabel.text = "\(meme.topText)...\(meme.bottomText)"
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
     
 }
 
---
+++ OnTheMap/ViewControllers/TabBarController.swift
@@ -0,0 +1,13 @@
+//
+//  TabBarController.swift
+//  OnTheMap
+//
+//  Created by Dilip Agheda on 27/2/22.
+//
+
+import Foundation
+import UIKit
+
+class TabBarController: UITabBarController {
+
+}
