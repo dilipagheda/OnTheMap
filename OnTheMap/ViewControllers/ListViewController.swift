044df6314f8d419223a0d45f1fc398e7a2679cd9
Dilip Agheda <dilip.agheda@origin.com.au>
2022-02-27 16:51:54 +1100

    setup a skeleton for tab view controller

---
+++ OnTheMap/ViewControllers/ListViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ListViewController.swift
+//  OnTheMap
+//
+//  Created by Dilip Agheda on 27/2/22.
+//
+
+import Foundation
+import UIKit
+
+class ListViewController: UIViewController {
+    
+}
+
---
+++ OnTheMap/ViewControllers/MapViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MapViewController.swift
+//  OnTheMap
+//
+//  Created by Dilip Agheda on 27/2/22.
+//
+
+import Foundation
+
+import UIKit
+
+class MapViewController: UIViewController {
+    
+}
+
